9c13bd2aaebcec70f868f84880916868699621d7
pejuangsejati1996 <46353506+pejuangsejati1996@users.noreply.github.com>
2019-01-04 00:09:47 +0700

    Add files via upload

---
+++ content.php
@@ -0,0 +1,20 @@
+<?php
+if ($_GET['id']==1){
+ echo "Sejarah Klaten tersebar diberbagai catatan arsip-arsip kuno dan kolonial, arsip-arsip kuno dan manuskrip Jawa. Catatan itu seperti tertulis dalam Serat Perjanjian Dalem Nata, Serat Ebuk Anyar, Serat Siti Dusun, Sekar Nawala Pradata, Serat Angger Gunung, Serat Angger Sedasa dan Serat Angger Gladag. Dalam bundel arsip Karesidenan Surakarta menjadikan rujukan sejarah Klaten seperti tercantum dalam Soerakarta Brieven van Buiten Posten, Brieven van den Soesoehoenan 1784-1810, Daghregister van den Resi dentie Soerakarta 1819, Reporten 1787-1816, Rijksblad Soerakarta dan Staatblad van Nederlandsche Indie. Babad Giyanti, Babad Bedhahipun Karaton Negari Ing Ngayogyakarta, Babad Tanah Jawi dan Babad Sindula menjadi sumber lain untuk menelusuri sejarah Klaten.";
+}
+elseif ($_GET['id']==2){
+ echo "Perayaan malam pergantian tahun 2018 menuju tahun 2019 di Kabupaten Klaten ada yang berbeda dari tahun lalu. Karena malam tahun baru yang biasanya diwarnai dengan pesta kembang itu diganti dengan…";
+}
+elseif ($_GET['id']==3){
+ echo "INI TRANSPARANSI";
+}
+elseif ($_GET['id']==4){
+ echo "Sejarah Klaten tersebar diberbagai catatan arsip-arsip kuno dan kolonial, arsip-arsip kuno dan manuskrip Jawa. Catatan itu seperti tertulis dalam Serat Perjanjian Dalem Nata, Serat Ebuk Anyar, Serat Siti Dusun, Sekar Nawala Pradata, Serat Angger Gunung, Serat Angger Sedasa dan Serat Angger Gladag. Dalam bundel arsip Karesidenan Surakarta menjadikan rujukan sejarah Klaten seperti tercantum dalam Soerakarta Brieven van Buiten Posten, Brieven van den Soesoehoenan 1784-1810, Daghregister van den Resi dentie Soerakarta 1819, Reporten 1787-1816, Rijksblad Soerakarta dan Staatblad van Nederlandsche Indie. Babad Giyanti, Babad Bedhahipun Karaton Negari Ing Ngayogyakarta, Babad Tanah Jawi dan Babad Sindula menjadi sumber lain untuk menelusuri sejarah Klaten.";
+}
+elseif ($_GET['id']==5){
+    echo "INI DATANG & KUNJUNGI";
+   }
+   elseif ($_GET['id']==6){
+    echo "INI PELAYANAN PUBLIC";
+   }
+?>
---
+++ javascript.js
@@ -0,0 +1,41 @@
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('jam').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+
+var tanggallengkap = new String();
+var namahari = ("Minggu Senin Selasa Rabu Kamis Jumat Sabtu");
+namahari = namahari.split(" ");
+var namabulan = ("Januari Februari Maret April Mei Juni Juli Agustus September Oktober November Desember");
+namabulan = namabulan.split(" ");
+var tgl = new Date();
+var hari = tgl.getDay();
+var tanggal = tgl.getDate();
+var bulan = tgl.getMonth();
+var tahun = tgl.getFullYear();
+tanggallengkap = namahari[hari] + ", " +tanggal + " " + namabulan[bulan] + " " + tahun;
+
+$(function() {
+  $('#loading').ajaxStart(function(){
+      $(this).fadeIn();
+  }).ajaxStop(function(){
+      $(this).fadeOut();
+  });
+
+  $('#menu a').click(function() {
+      var url = $(this).attr('href');
+      $('#container').load(url);
+      return false;
+  });
+});
